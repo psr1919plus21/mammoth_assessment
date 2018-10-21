485e7a0a9d194c486da621293671a771f1191e3d
psr1919plus21 <psr1919plus21@gmail.com>
2018-10-21 16:45:01 +0300

    :rocket: Add module for random string EN,RU generating.

---
+++ randomString.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import string
+import random
+
+cyrillic = ["а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"]
+def create_random_string(size=6, chars=cyrillic + list(string.ascii_letters) + list(string.digits)):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+random_string = create_random_string()
+print(random_string)
